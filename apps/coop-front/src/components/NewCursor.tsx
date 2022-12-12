89c515ba12a2a1842ffa5d1ec5f4131168c0f336
JSC <gbs04087@gmail.com>
2022-12-12 22:45:34 +0900

    feat: change cursor position

--- apps/coop-front/src/components/NewCursor.tsx
+++ apps/coop-front/src/components/NewCursor.tsx
@@ -16,7 +16,7 @@ const NewCursor: CursorComponent = ({id,color}) => {
         <div style={{
             position:"relative"
             }}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" fill="none" fillRule="evenodd">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" fill="none" fillRule="evenodd">
                 <g fill="rgba(0,0,0,.2)" transform="translate(1,1)">
                 <path d="m12 24.4219v-16.015l11.591 11.619h-6.781l-.411.124z" />
                 <path d="m21.0845 25.0962-3.605 1.535-4.682-11.089 3.686-1.553z" />
@@ -29,14 +29,28 @@ const NewCursor: CursorComponent = ({id,color}) => {
                 <path d="m19.751 24.4155-1.844.774-3.1-7.374 1.841-.775z" />
                 <path d="m13 10.814v11.188l2.969-2.866.428-.139h4.768z" />
                 </g>
-            </svg>
+            </svg> */}
+            <svg
+        className="cursor"
+        width="24"
+        height="36"
+        viewBox="0 0 24 36"
+        fill="none"
+        stroke="white"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z"
+          fill={color}
+        />
+      </svg>
             <div
                 style={{
                 backgroundColor: color,
                 borderRadius: 4,
                 position: "absolute",
-                top: 20,
-                left: 10,
+                top: 14,
+                left: 4,
                 padding: "5px 10px"
                 }}
             >
