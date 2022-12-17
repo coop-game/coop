ae16213b36a040f1179bd38ec88d67861c5d208a
JSC <gbs04087@gmail.com>
2022-12-17 22:59:24 +0900

    feat: add translate kr(main) with en

---
+++ apps/coop-front/pages/ErrorPage.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+type ErrorPagePropsType = {
+  statusCode: string | null;
+  errorMessage: string | null;
+};
+
+const ErrorPage = ({ statusCode, errorMessage }: ErrorPagePropsType) => {
+  const router = useRouter();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      router.push("/");
+    }, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [router]);
+  return (
+    <div>
+      <div>statusCode</div>
+      <div>{statusCode}</div>
+      <div>errorMessage</div>
+      <div>{errorMessage}</div>
+      <Button onClick={() => router.push("/")}>빠르게 돌아가기</Button>
+    </div>
+  );
+};
+export default ErrorPage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      statusCode: context?.query?.statusCode ?? null,
+      errorMessage: context?.query?.errorMessage ?? null,
+    }, // will be passed to the page component as props
+  };
+}
