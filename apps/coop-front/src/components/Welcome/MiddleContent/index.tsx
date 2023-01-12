e856c74180003116a2a8bcfd744534046b041942
정진 <supermilktank8@gmail.com>
2023-01-12 22:34:30 +0900

    feat: welcome page css

--- apps/coop-front/src/components/Welcome/MiddleContent/index.tsx
+++ apps/coop-front/src/components/Welcome/MiddleContent/index.tsx
@@ -1,13 +1,15 @@
-import { Box, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import MiddleContentPictureSlide from "./src/contents";
 import MiddleContentScrollDetector from "./src/detect";
 import stock1 from "../../../../src/asset/stock1.jpg";
 import stock2 from "../../../../src/asset/stock2.jpg";
 import stock3 from "../../../../src/asset/stock3.jpg";
 import stock4 from "../../../../src/asset/stock4.jpg";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { nextContentType } from "@pages/welcome";
+import Image from "next/image";
+import { css } from "@emotion/react";
 
 function MiddleContent({ nextContent }: { nextContent: nextContentType }) {
   const [nowDetect, setNowDetect] = useState<number>(1);
@@ -17,6 +19,32 @@ function MiddleContent({ nextContent }: { nextContent: nextContentType }) {
     setNowDetect((prev) => {
       return index;
     });
+  };
+  const pencilVariants: Variants = {
+    offscreen: {
+      x: `-300%`,
+    },
+    onscreen: {
+      x: `-50%`,
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 1,
+      },
+    },
+  };
+  const starVariants: Variants = {
+    offscreen: {
+      scale: 0,
+    },
+    onscreen: {
+      scale: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.8,
+        duration: 2,
+      },
+    },
   };
   return (
     <Box
@@ -32,6 +60,7 @@ function MiddleContent({ nextContent }: { nextContent: nextContentType }) {
           display="flex"
           justifyContent="center"
           width="100%"
+          height="100%"
           sx={{
             top: `${
               nextContent.detect && nextContent.ratio !== undefined
@@ -40,9 +69,57 @@ function MiddleContent({ nextContent }: { nextContent: nextContentType }) {
             }px`,
           }}
         >
-          <Text w="100%" left="0px" fontSize="6xl" maxWidth="1024px">
-            그린다
-          </Text>
+          <Flex flexDirection="column" w="100%" h="100%" position="relative">
+            <motion.div
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 1 }}
+              css={css`
+                position: absolute;
+                top: -1%;
+                left: 30%;
+                @media (max-width: 490px) {
+                  left: 40%;
+                }
+                @media (min-width: 760px) {
+                  left: 15%;
+                }
+                @media (min-width: 1000px) {
+                  left: 10%;
+                }
+                @media (min-width: 1500px) {
+                  left: 8%;
+                }
+              `}
+            >
+              <motion.div variants={starVariants}>
+                <Image
+                  src="/images/writingToolsIcon/star_red_rotate.png"
+                  alt="연필"
+                  width={"50"}
+                  height={"50"}
+                ></Image>
+              </motion.div>
+            </motion.div>
+            <Text w="100%" left="0px" fontSize="6xl" maxWidth="1024px">
+              그린다
+            </Text>
+
+            <motion.div
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ amount: 0.8 }}
+            >
+              <motion.div variants={pencilVariants}>
+                <Image
+                  src="/images/writingToolsIcon/long_pencil_colorful.png"
+                  alt="연필"
+                  width={"2000"}
+                  height={"250"}
+                ></Image>
+              </motion.div>
+            </motion.div>
+          </Flex>
           <Box position="absolute" w="100%" top="15vh">
             <MiddleContentPictureSlide index={nowDetect} images={images} />
           </Box>
