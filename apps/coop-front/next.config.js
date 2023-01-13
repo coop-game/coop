4fd537a9447954c764be45adc0971b107b68a975
JSC <gbs04087@gmail.com>
2023-01-13 18:49:08 +0900

    refactor: next.config

--- apps/coop-front/next.config.js
+++ apps/coop-front/next.config.js
@@ -3,8 +3,7 @@ const path = require("path");
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    prependData: `
-    `,
+    prependData: ``,
   },
   experimental: {
     transpilePackages: [
