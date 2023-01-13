c7cd3b592f9c9abcde3fc2c31770c2eed5e25e1b
JSC <gbs04087@gmail.com>
2023-01-13 18:03:14 +0900

    refactor: change upper case component name

---
+++ apps/coop-front/src/components/relay-race/Wating.tsx
@@ -0,0 +1,45 @@
+import { Box } from "@chakra-ui/react";
+import Chatting from "@components/Chatting";
+import Progress from "@components/Progress";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+const Wating = ({
+  isPlay,
+  setIsPlay,
+  startTime,
+}: {
+  isPlay: "running" | "paused";
+  setIsPlay: Dispatch<SetStateAction<"running" | "paused">>;
+  startTime: number;
+}) => {
+  useEffect(() => {
+    if (isPlay === "paused") {
+      setIsPlay("running");
+    }
+  }, [isPlay, setIsPlay]);
+  return (
+    <Box
+      w="100%"
+      height="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Progress
+        time={50000}
+        callback={() => {}}
+        play={isPlay}
+        startTime={startTime}
+      />
+      <div>다른 사람들이 문제를 풀고 있습니다. 대기 해주십시오.</div>
+      <Box w="100%" height="500px" display="flex" justifyContent="center">
+        <Box w="100%" maxW="800px" height="500px">
+          <Chatting />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Wating;
