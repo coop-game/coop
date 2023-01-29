a66585f5f43e2dd4daa63467283187721e7c5afd
JSC <gbs04087@gmail.com>
2023-01-29 17:11:12 +0900

    feat: resize put pin of postit

--- apps/coop-front/src/components/Animation/PutPin.tsx
+++ apps/coop-front/src/components/Animation/PutPin.tsx
@@ -8,35 +8,42 @@ const PutPin = () => {
   return (
     <div
       css={css`
-        width: 130px;
-        height: 130px;
-        border-radius: 0px 0px 0px 95px;
-        overflow: hidden;
-        position: relative;
-        @keyframes movePin {
-          100% {
-            transform: translate(0px, 30px);
-          }
-        }
-        & > .pin {
-          transform: translate(20px, 10px);
-          & > img {
-            filter: drop-shadow(3px 3px 3px #4d4c4c);
-          }
-        }
-        & > .move {
-          animation: movePin 0.2s ease-out;
-          animation-fill-mode: forwards;
-        }
+        transform-origin: 0, 0;
+        transform: scale(0.4);
       `}
     >
-      <div className={"pin" + (isAnimationend === true ? " move" : "")}>
-        <Image
-          src={"/images/pin.png"}
-          width={100}
-          height={100}
-          alt={""}
-        ></Image>
+      <div
+        css={css`
+          width: 130px;
+          height: 130px;
+          border-radius: 0px 0px 0px 95px;
+          overflow: hidden;
+          position: relative;
+          @keyframes movePin {
+            100% {
+              transform: translate(0px, 30px);
+            }
+          }
+          & > .pin {
+            transform: translate(20px, 10px);
+            & > img {
+              filter: drop-shadow(3px 3px 3px #4d4c4c);
+            }
+          }
+          & > .move {
+            animation: movePin 0.2s ease-out;
+            animation-fill-mode: forwards;
+          }
+        `}
+      >
+        <div className={"pin" + (isAnimationend === true ? " move" : "")}>
+          <Image
+            src={"/images/pin.png"}
+            width={100}
+            height={100}
+            alt={""}
+          ></Image>
+        </div>
       </div>
     </div>
   );
